2d8d5a3c162790bfb2c7bf05886417a28f070936
Victor Romero <romeroa.victor@yahoo.com>
2020-09-11 23:15:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -18,7 +18,7 @@ new_cart = cart.dup
     counter= 0
     item_counter = 0
     while counter < new_cart.length do
-      if hash == new_cart[item_counter]
+      if hash == new_cart[counter]
         hash[:count] = item_counter + 1
         item_counter = item_counter +1
       end
